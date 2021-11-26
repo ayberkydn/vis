9d2c76659a3719f6969e4586c4d927a3c9ad2fd3
Ayberk Aydin <ayberkydn@gmail.com>
2021-11-26 16:46:49 +0300

    upd

--- src/main.py
+++ src/main.py
@@ -25,7 +25,7 @@ from src.debug import (
     pixel_sample_ratio_map,
 )
 
-IMG_SIZE = 256
+IMG_SIZE = 512
 NET_INPUT_SIZE = 224
 
 input_img_layer = InputImageLayer(
@@ -37,42 +37,43 @@ input_img_layer = InputImageLayer(
 
 aug_fn = torch.nn.Sequential(
     RandomCircularShift(),
-    kornia.augmentation.RandomRotation(
-        degrees=90,
-        same_on_batch=False,
-        p=1,
-    ),
+    # kornia.augmentation.RandomRotation(
+    #     degrees=90,
+    #     same_on_batch=False,
+    #     p=1,
+    # ),
     kornia.augmentation.RandomResizedCrop(
-        size=(224, 224),
+        size=(NET_INPUT_SIZE, NET_INPUT_SIZE),
         scale=(0.1, 1),
         ratio=(0.8, 1.2),  # aspect ratio
         same_on_batch=False,
     ),
-    kornia.augmentation.RandomPerspective(
-        scale=0.5,
-        p=1,
-        same_on_batch=False,
-    ),
-    kornia.augmentation.RandomHorizontalFlip(),
-    kornia.augmentation.RandomVerticalFlip(),
+    # kornia.augmentation.RandomPerspective(
+    #     distortion_scale=0.5,
+    #     p=1,
+    #     same_on_batch=False,
+    # ),
+    # kornia.augmentation.RandomHorizontalFlip(),
+    # kornia.augmentation.RandomVerticalFlip(),
 )
 
 
 network_names = [
-    "densenet121",  # good
-    "resnet50",  # good
-    "efficientnet_b4",  # bad
-    "inception_v4",  # good
-    "vit_base_patch16_224",  # meh
+    # "densenet121",  # good
+    # "resnet50",  # good
+    "resnet18",  # good
+    # "efficientnet_b4",  # bad
+    # "inception_v4",  # good
+    # "vit_base_patch16_224",  # meh
 ]
 
 networks = get_timm_networks(network_names)
-optimizer = torch.optim.Adam(input_img_layer.parameters(), lr=0.02)
+optimizer = torch.optim.Adam(input_img_layer.parameters(), lr=0.025)
 
 
 #%% train
-ITERATIONS = 1000
-BATCH_SIZE = 8
+ITERATIONS = 100
+BATCH_SIZE = 16
 for TARGET_CLASS in [309]:
     for n in tqdm.tqdm(range(ITERATIONS)):
         net = random.choice(networks)
@@ -82,7 +83,7 @@ for TARGET_CLASS in [309]:
 
         prob_loss = probability_maximizer_loss(out, TARGET_CLASS)
         score_loss = score_maximizer_loss(out, TARGET_CLASS)
-        loss = (prob_loss + prob_loss) / 2
+        loss = score_loss
         loss.backward()
 
         optimizer.step()
